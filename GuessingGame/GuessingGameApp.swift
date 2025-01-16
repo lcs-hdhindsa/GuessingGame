de5ead52588a1872dbcfd37058254961e31a7923
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-01-16 14:27:29 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Harshan Dhindsa on 2025-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
